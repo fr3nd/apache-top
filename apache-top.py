ae66562ab876b87ed4888ae6d1ac2cce9287901c
Carles Amigo <fr3nd@fr3nd.net>
2012-10-28 10:37:07 +0100

    Code cleanup: Removing trailing spaces

--- apache-top.py
+++ apache-top.py
@@ -1,23 +1,23 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-# 
+#
 # apache-top
 # Copyright (C) 2006  Carles Amigó
-# 
+#
 # This program is free software; you can redistribute it and/or
 # modify it under the terms of the GNU General Public License
 # as published by the Free Software Foundation; either version 2
 # of the License, or (at your option) any later version.
-# 
+#
 # This program is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU General Public License for more details.
-# 
+#
 # You should have received a copy of the GNU General Public License
 # along with this program; if not, write to the Free Software
 # Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
-# 
+#
 
 from HTMLParser import HTMLParser
 import operator
@@ -33,20 +33,20 @@ class ApacheStatusParser(HTMLParser):
     """
     Clase que parseja la sortida del handler server-status de apache
     """
-    
+
     performance_info = 2
     scoreboard = 3
     proceses = 4
-    
+
     status = 0
-    
+
     store = False # defineix si el contingut s'ha de guardar o no
     append = False # defineix si els seguents caracters s'han d'afegir o posar en un altre camp
 
     performance_info_data = []
     scoreboard_data = []
     proceses_data = []
-    
+
     def __init__(self):
         HTMLParser.__init__(self)
         self.performance_info_data = []
@@ -74,7 +74,7 @@ class ApacheStatusParser(HTMLParser):
                 else:
                     if len(self.proceses_data[-1]) > 0:
                         self.proceses_data.append([])
-                
+
             elif tag == "td":
                 self.store = True
 
@@ -101,7 +101,7 @@ class ApacheStatusParser(HTMLParser):
                     self.proceses_data[-1].append(data.replace("\n",""))
                 else:
                     self.proceses_data[-1][-1] += data.replace("\n","")
-    
+
     def handle_charref(self, ref):
         self.append = True
         self.handle_data("&#%s;" % ref)
@@ -109,7 +109,7 @@ class ApacheStatusParser(HTMLParser):
     def handle_entityref(self, ref):
         self.append = True
         self.handle_data("&%s;" % ref)
-        
+
     def eval_data(self):
         for process in self.proceses_data:
             # PID
@@ -141,20 +141,18 @@ def usage(exit = 1):
     print main.__doc__
     sys.exit(exit)
 
-        
 def print_screen(screen, url, show_scoreboard):
-    
     screen = stdscr.subwin(0, 0)
     screen.nodelay(1)
-    
+
     end = False
     sort = 5
-    message = "" 
+    message = ""
     reverse = True
     show_only_active = True
     y = 0
     c = ""
-    
+
     while not end:
         try:
             data = ApacheStatusParser()
@@ -170,7 +168,7 @@ def print_screen(screen, url, show_scoreboard):
             screen.addstr(0,0,data.performance_info_data[5].replace("Server uptime: ","Uptime:").replace(" days","d").replace(" day","d").replace(" hours","h").replace(" hour","h").replace(" minutes","m").replace(" minute","m").replace(" seconds","s").replace("second","s") + ", " + data.performance_info_data[3])
             screen.addstr(1,0,data.performance_info_data[7])
             screen.addstr(2,0,data.performance_info_data[8].replace("request","req").replace("second","sec") + ", Active/Idle: " + data.performance_info_data[9].split()[0] + "/" + data.performance_info_data[9].split()[5])
-            
+
             # evaluar scoreboard
             if show_scoreboard:
                 y = 6
@@ -180,26 +178,26 @@ def print_screen(screen, url, show_scoreboard):
                 screen.addstr(6,0,"  |  C Closing connection, L Logging, G Gracefully finishing       |")
                 screen.addstr(7,0,"  |  I Idle cleanup of worker, . Open slot with no current process |")
                 screen.addstr(8,0,"  +----------------------------------------------------------------+")
-            
+
             # imprimim el scoreboard
             for num in range(0,len(data.scoreboard_data[0]),width):
                  screen.addstr(y+4+num/width,0, data.scoreboard_data[0][num:num+width])
-        
+
             if len(message) > 0:
                 screen.addstr(y+5+num/width,0,message, curses.A_BOLD | curses.A_REVERSE)
                 message = ""
-                        
+
             print_proceses(y+6+num/width,0,screen, data.proceses_data, columns=[ 1, 3, 5, 4, 11, 10, 12 ], sort=sort, reverse=reverse, width=width, show_only_active=show_only_active )
-    
+
             #screen.hline(2, 1, curses.ACS_HLINE, 77)
             screen.refresh()
             time.sleep(2)
-            
+
             try:
                 c = screen.getkey()
             except:
                 pass
-            
+
             if c == "q":
                 # sortir
                 end = True
@@ -257,20 +255,18 @@ def print_screen(screen, url, show_scoreboard):
                     reverse = True
                     message = "Normal sorting"
             c = ""
-            
+
 	except IndexError:
 	    raise
         except:
             pass
-        
-        
 
 def print_proceses(y,x,screen, proceses, columns, sort, reverse, width, show_only_active = True):
     header = "PID   M SS     CPU  VHost           IP              Request"
     screen.addstr(y,x,header + " "*(width-len(header)), curses.A_REVERSE)
-    
+
     n = 1
-    
+
     if sort != None:
         for process in sorted(proceses, key=operator.itemgetter(sort), reverse=reverse):
             n += print_process(y+n,x,screen,process,columns,show_only_active,width)
@@ -293,18 +289,18 @@ def print_process(y,x,screen,process,columns,show_only_active,width):
             n = n+ 2
             screen.addstr(y,n, str(process[columns[2]])) # PID
             n = n+ 6
-            
+
             cpu = str(process[columns[3]])
             if len(cpu.split('.')[1]) < 2:
                 cpu = cpu + "0"*(2-len(cpu.split('.')[1]))
             screen.addstr(y,n+(4-len(cpu)), cpu) # CPU
-            
+
             n = n+ 6
             screen.addstr(y,n, str(process[columns[4]])) # VHOST
-            
+
             n = n+ 16
             screen.addstr(y,n, str(process[columns[5]])) # IP
-            
+
             n = n+ 15
             screen.addstr(y,n, " " + str(process[columns[6]])) # REQUEST
             return 1
@@ -312,14 +308,11 @@ def print_process(y,x,screen,process,columns,show_only_active,width):
             return 1
     else:
         return 0
-    
-
-
 
 def main(url, stdscr, show_scoreboard):
-    """Shows the actual status of the Apache web server using the server-status 
+    """Shows the actual status of the Apache web server using the server-status
 url. It needs the ExtendedStatus flag
-    
+
     Usage: apache-top [-s] -u url
         -u url    Url where apache-status is located
 		  Example: apache-top.py -u http://www.domain.com/server-status
@@ -338,9 +331,9 @@ url. It needs the ExtendedStatus flag
 	s	Show/Hide mod_status scoreboard
 	a	Switch between show all processes and show only active processes (default)
 	r	Reverse sort
-	
+
     """
-    
+
     cols = {
         "srv":    0,
         "pid":    1,
@@ -356,9 +349,7 @@ url. It needs the ExtendedStatus flag
         "vhost":    11,
         "request":    12
     }
-    
 
-    
     try:
     	print_screen(stdscr,url,show_scoreboard)
     except:
@@ -366,14 +357,14 @@ url. It needs the ExtendedStatus flag
 
 
 if __name__ == "__main__":
-    
+
     url = None
     show_scoreboard = False
     try:
         opt_list = getopt.getopt(sys.argv[1:], "u:h:s")
     except:
         usage()
-        
+
     for opt in opt_list[0]:
         if opt[0]=="-h":
             usage(0)
@@ -383,11 +374,11 @@ if __name__ == "__main__":
             url = opt[1]
         else:
             usage
-    
+
     if url == None:
         print "*** ERROR: Url missing\n"
         usage()
-        
+
     try:
         # Initialize curses
         stdscr=curses.initscr()
@@ -409,8 +400,8 @@ if __name__ == "__main__":
         stdscr.keypad(0)
         curses.echo()
         curses.nocbreak()
-        curses.endwin()                 # Terminate curses 
-        
+        curses.endwin()                 # Terminate curses
+
     except:
         # In event of error, restore terminal to sane state.
         stdscr.keypad(0)
